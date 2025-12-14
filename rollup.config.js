f376b204e391a7198fb0551336180e599c860be3
Nico Rehwaldt <git_nikku@nixis.de>
2025-12-15 00:07:14 +0100

    chore!: drop UMD distribution

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-import { terser } from 'rollup-plugin-terser';
-
 import pkg from './package.json';
 
 function pgl(plugins=[]) {
@@ -15,30 +13,10 @@ function pgl(plugins=[]) {
 
 const srcEntry = pkg.source;
 
-const umdDist = pkg['umd:main'];
-
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: srcEntry,
-    output: {
-      file: umdDist.replace(/\.js$/, '.prod.js'),
-      format: 'umd',
-      name: 'AsyncDidi'
-    },
-    plugins: pgl([
-      terser()
-    ])
-  },
   {
     input: srcEntry,
     output: [
-      {
-        file: umdDist,
-        format: 'umd',
-        name: 'AsyncDidi'
-      },
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
     ],
--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -56,17 +56,4 @@ describe('integration', function() {
 
   });
 
-
-  describe('umd bundle', function() {
-
-    var didi = require('../../dist/async-didi.umd.prod.js');
-
-    it('should expose API', function() {
-      expect(didi.annotate).to.exist;
-      expect(didi.AsyncInjector).to.exist;
-      expect(didi.Module).to.exist;
-    });
-
-  });
-
 });
